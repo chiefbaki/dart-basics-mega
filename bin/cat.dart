36ebb58a2a7c0268f6567fec2a6a11819784a80b
Islam <ik509331@gmail.com>
2023-10-29 14:33:40 +0300

    modified car.dart

--- bin/cat.dart
+++ bin/cat.dart
@@ -2,52 +2,52 @@ import 'dart:io';
 import 'package:dio/dio.dart';
 import 'package:translator/translator.dart';
 
-
 void main(){
-  //await getHttp().then((value) => print(value));
   perfomance();
 }
 
-void translate(String textCat) async{
+Future<String> translate(String textCat, String lang) async {
   final translator = GoogleTranslator();
-  translator.translate(textCat, from: "en", to: "ru").then((value) => print(value));
+  final translation = await translator.translate(textCat, from: "en", to: lang);
+  return translation.text;
 }
 
-Future<String> getHttp() async{
+Future<String> getHttp(String lang) async {
   final dio = Dio();
   final response = await dio.get("https://catfact.ninja/fact");
-  return response.data["fact"];
+  return translate(response.data["fact"], lang);
 }
 
-Future<void> perfomance() async{
-  bool flag = true;
+Future<void> perfomance() async {
   List<String> likedFacts = [];
-  String? fact = await getHttp();
-  print(fact);
-  while( flag ){
-    String userAnswer = inputHandler("Choose one option: 1-(liked) 2-(further) 3-(favourite facts): ");
-    switch(userAnswer){
-    case "1":
-      if (fact != null) {
+  String lang = inputHandler("Choose lang: en, fr, ky: ");
+  while (true) {
+    String? fact = await getHttp(lang);
+    print(fact);
+    String userAnswer = inputHandler(
+        "\nChoose one option: 1-(liked) 2-(further) 3-(favourite facts) 4-exit: ");
+    switch (userAnswer) {
+      case "1":
         likedFacts.add(fact);
-      }
-      break;
-    case "2":
-      String fact = await getHttp();
-      print(fact);
-      String likeAnswer = inputHandler("Did you like this fact? yes/no: ");
-      if (likeAnswer.toLowerCase() == "yes") {
-        likedFacts.add(fact);
-      }
-      break;
-    case "3":
-      print(likedFacts);
-      break;
+      case "2":
+        String fact = await getHttp(lang);
+        print(fact);
+        String likeAnswer = inputHandler("Did you like this fact? yes/no: ");
+        if (likeAnswer.toLowerCase() == "yes") {
+          likedFacts.add(fact);
+        }
+      case "3":
+        for (var i in likedFacts) {
+          print("\n$i");
+        }
+        break;
+      case '4':
+        exit(0);
     }
   }
 }
 
-String inputHandler(String text){
+String inputHandler(String text) {
   stdout.write(text);
   return stdin.readLineSync()!;
 }
